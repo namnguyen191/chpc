858564fd87b80e70dfbab7e51e565a34fab91bb2
Noele106 <112039618+Noele106@users.noreply.github.com>
2023-04-03 22:44:52 -0400

    Added javadocs for Statistics package

--- app/src/main/java/chpc/statisticaltest/StatisticsResults.java
+++ app/src/main/java/chpc/statisticaltest/StatisticsResults.java
@@ -2,16 +2,27 @@ package chpc.statisticaltest;
 
 import org.apache.commons.math3.stat.descriptive.SummaryStatistics;
 
+/**
+ * Class for getting statistics from a dataset
+ */
 public class StatisticsResults implements StatsPlug{
 
     SummaryStatistics apacheStats = new SummaryStatistics();
 
+    /**
+     * Load values into SummaryStatistics object in StatisticsResult
+     * @Wparam values Array of doubles representing values meant to be inserted for statistical processing
+     */
     public void loadValues(double[] values){
         for(double entry : values){
             apacheStats.addValue(entry);
         }
     }
 
+    /**
+     * Method to get statistics after loading in values
+     * @return string containing results
+     */
     public String getStats(){
         String result = "";
         result += "Mean: " + apacheStats.getMean() + "\n";
--- app/src/main/java/chpc/statisticaltest/StatsPlug.java
+++ app/src/main/java/chpc/statisticaltest/StatsPlug.java
@@ -1,5 +1,9 @@
 package chpc.statisticaltest;
 
+/**
+ * Interface for statistics
+ * Any object implementing this interface can be used in lieu of StatisticsResults to customise the statistics that the user wants
+ */
 public interface StatsPlug {
     public void loadValues(double[] values);
     public String getStats();
